3c095c927637a1f7f2aab34f2b71436d5d5a6bc0
Bodo Schulz <bodo@boone-schulz.de>
2021-02-23 19:39:03 +0100

    update molecule

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,26 +1,71 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import pytest
-import re
-import os
-import yaml
-import json
-# import subprocess
-from pyjolokia import Jolokia, JolokiaError
-from errno import ECONNREFUSED
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
 
-# import testinfra
+import pytest
+import os
+# import re
+# import yaml
+# import json
+
+# from pyjolokia import Jolokia, JolokiaError
+# from errno import ECONNREFUSED
+
 import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def base_directory():
+    """ ... """
+    cwd = os.getcwd()
+
+    if('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
+"""
+    parse ansible variables
+    - defaults/main.yml
+    - vars/main.yml
+    - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
+"""
+
+
 @pytest.fixture()
-def AnsibleDefaults():
-    with open("../../defaults/main.yml", 'r') as stream:
-        return yaml.load(stream)
+def get_vars(host):
+    """ ... """
+    base_dir, molecule_dir = base_directory()
+
+    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
+    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
+    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
 
 @pytest.mark.parametrize("dirs", [
     "/etc/ansible/facts.d",
@@ -68,7 +113,7 @@ def test_service(host):
 def test_open_port(host, ports):
 
     for i in host.socket.get_listening_sockets():
-        print( i )
+        print(i)
 
     application = host.socket("tcp://%s" % (ports))
     assert application.is_listening
